0292fdd519f16ce5cbb5258f9682ff8d8a728754
PierreMo <pierremouries05@gmail.com>
2026-02-17 20:50:30 +0100

    initial flask test

---
+++ setup.sh
@@ -0,0 +1,17 @@
+cd ~
+cd Documents/
+
+sudo apt install git
+sudo apt install python3
+
+git clone https://github.com/PierreMo/ADR
+
+python3 -m pip install --upgrade pip
+
+python3 pip install python3-venv
+python3 -m venv env
+source ./env/bin/activate
+
+python3 -m pip install requests
+
+python3 ./ADR/client.py
