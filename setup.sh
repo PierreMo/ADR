f3788482cf3e10dbff099f0302e21332f3cfeeae
PierreMo <pierremouries05@gmail.com>
2026-03-24 17:29:12 +0100

    updating setup.sh from the raspberry

--- setup.sh
+++ setup.sh
@@ -1,23 +1,24 @@
 cd ~
 cd Documents/
 
-# sudo apt install nmcli
-# nmcli dev wifi
-sudo apt install wpasupplicant
-wpa_passphrase LAPTOP-PM moijepeux > /etc/wpa_supplicant.conf
-wpa_supplicant
-
+echo "----- INSTALLING GIT AND PYTHON -----"
 sudo apt install git
 sudo apt install python3
 
+echo "----- CLONING THE REPOSITORY -----"
 git clone https://github.com/PierreMo/ADR
 
+echo "----- INSTALLING PIP -----"
 python3 -m pip install --upgrade pip
 
+echo "----- INSTALLING VENV AND SETTING IT UP -----"
 python3 pip install python3-venv
 python3 -m venv env
 source ./env/bin/activate
 
+echo "----- INSTALLING COMMUNICATIONS LIBs -----"
 python3 -m pip install socket
+python3 -m pip install datetime
 
+echo "----- RUNNING THE CLIENT PROGRAM -----"
 python3 ./ADR/client.py
