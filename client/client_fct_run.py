25eda086acce63305adcf295f8bf4c10c9441351
PierreMo <pierremouries05@gmail.com>
2026-04-01 14:48:40 +0200

    client loop stopped with q and showing how many packets are delaying

--- client/client_fct_run.py
+++ client/client_fct_run.py
@@ -1,11 +1,15 @@
 from datetime import datetime
 import time
+import select
+import sys
 
 from __init__ import CLIENT_DEBUG, PACKET_PER_SECOND
 
 def run(s):
     print("Starting radar...")
     microsec_btwn_packets = 1000000 / PACKET_PER_SECOND
+    nb_packets_delaying = 0
+
     # running loop of the radar
     while True:
         #record time for packet per seconds
@@ -24,3 +28,14 @@ def run(s):
         time_delta = datetime.now().microsecond - begin_time.microsecond
         if time_delta < microsec_btwn_packets:
             time.sleep((microsec_btwn_packets - time_delta) / 1000000.0)
+        else:
+            nb_packets_delaying += 1
+
+        # Non-blocking check for keyboard input
+        if select.select([sys.stdin], [], [], 0)[0]:
+            user_input = sys.stdin.readline().strip().lower()
+            if user_input == 'q':
+                print("Loop stopped by user.")
+                break
+
+    print(f"Packets late: {nb_packets_delaying}")
